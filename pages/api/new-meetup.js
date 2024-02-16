2a4e593f348309e554180c90f321d4aa5b59b1f7
주비 <124552101+JubiJung@users.noreply.github.com>
2024-02-16 19:08:50 +0900

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,7 +4,7 @@ async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     const client = await MongoClient.connect(
-      "mongodb+srv://jubi1838:5BnpzlPnZf6GbPq9@cluster0.fmfz8dt.mongodb.net/connect?retryWrites=true&w=majority"
+      "mongodb+srv://jubi1838:<password>@cluster0.fmfz8dt.mongodb.net/connect?retryWrites=true&w=majority"
     );
     const db = client.db();
     const connectCollection = db.collection("connect");
